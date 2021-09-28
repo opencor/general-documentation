c6938f50e4738b18901d772ecdb3628b4724139c
Alan Garny <agarny@hellix.com>
2021-09-29 11:42:46 +1300

    Build the general libOpenCOR and OpenCOR documentations.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -24,19 +24,65 @@ macro(build_documentation PROJECT_NAME)
 
     set(PROJECT_NAME ${PROJECT_NAME})
 
+    string(TOLOWER ${PROJECT_NAME} PROJECT_REPOSITORY_NAME)
+
     if("${PROJECT_NAME}" STREQUAL "libOpenCOR")
+        set(PROJECT_ADDITIONAL_TEAM_MEMBERS)
         set(PROJECT_URL "https://opencor.ws/libopencor/index.html")
         set(PROJECT_DESCRIPTION "the backend library to <a href=\"https://opencor.ws/\">OpenCOR</a>, an <a href=\"licensing.html\">open source</a> <a href=\"supportedPlatforms.html\">cross-platform</a> modelling environment")
-        set(PROJECT_TUTORIAL)
+        set(PROJECT_LICENSE "<a href=\"https://opensource.org/licenses/Apache-2.0\">Apache v2.0 license</a>")
+        set(PROJECT_TUTORIAL_LISTING_ENTRY)
+        set(PROJECT_TUTORIAL_MENU_ENTRY)
+
+        set(ADDITIONAL_COPIED_FILES)
     else()
+        set(PROJECT_ADDITIONAL_TEAM_MEMBERS "
+                <tr>
+                    <td>
+                        <img src=\"res/pics/davidBrooks.jpg\" width=60 height=70 alt=\"David Brooks\">
+                    </td>
+                    <td>
+                        <div>
+                            Senior developer
+                        </div>
+
+                        <a href=\"mailto: David Brooks <d.brooks@auckland.ac.nz>?subject=About ${PROJECT_NAME}\">David Brooks</a>
+                    </td>
+                </tr>")
         set(PROJECT_URL "https://opencor.ws/")
         set(PROJECT_DESCRIPTION "an <a href=\"licensing.html\">open source</a> <a href=\"supportedPlatforms.html\">cross-platform</a> modelling environment (and a replacement for <a href=\"cor/index.html\">COR</a>)")
-        set(PROJECT_TUTORIAL "
+        set(PROJECT_LICENSE "<a href=\"https://opensource.org/licenses/GPL-3.0\">GPL v3.0 license</a>")
+        set(PROJECT_TUTORIAL_LISTING_ENTRY "
             <ul>
                 <li><a href=\"https://tutorial-on-cellml-opencor-and-pmr.readthedocs.io/en/latest/index.html\">OpenCOR tutorial</a></li>
             </ul>
 ")
+        set(PROJECT_TUTORIAL_MENU_ENTRY "
+                                { \"separator\": true },
+                                { \"level\": 0, \"label\": \"OpenCOR tutorial\", \"link\": \"https://tutorial-on-cellml-opencor-and-pmr.readthedocs.io/en/latest/index.html\", \"directLink\": true },")
+
+        set(ADDITIONAL_COPIED_FILES
+            res/pics/davidBrooks.jpg
+        )
     endif()
 
-    configure_file(${ABSOLUTE_DESTINATION_DIR}/index.html.in ${CMAKE_BINARY_DIR}/${RELATIVE_DESTINATION_DIR}/${ARGN}/index.html)
+    foreach(CONFIGURED_FILE downloads/index.html
+                            downloads/previousSnapshots.html
+                            res/menu.js
+                            contactUs.html
+                            index.html
+                            licensing.html
+                            supportedPlatforms.html
+                            team.html)
+        configure_file(${ABSOLUTE_DESTINATION_DIR}/${CONFIGURED_FILE}.in ${CMAKE_BINARY_DIR}/${RELATIVE_DESTINATION_DIR}/${ARGN}/${CONFIGURED_FILE})
+    endforeach()
+
+    foreach(COPIED_FILE downloads/index.js
+                        downloads/previousSnapshots.js
+                        res/pics/logo.png
+                        supportedPlatforms.js
+                        whatIsNew.js
+                        ${ADDITIONAL_COPIED_FILES})
+        configure_file(${ABSOLUTE_DESTINATION_DIR}/${COPIED_FILE}.${PROJECT_REPOSITORY_NAME} ${CMAKE_BINARY_DIR}/${RELATIVE_DESTINATION_DIR}/${ARGN}/${COPIED_FILE} COPYONLY)
+    endforeach()
 endmacro()
