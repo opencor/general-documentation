ee5ee1185e37109593e22564a4887315d5656ec6
Alan Garny <agarny@hellix.com>
2021-09-28 15:21:45 +1300

    Configure the index.html file.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,4 +1,6 @@
-macro(build_documentation DOCUMENTATION_NAME)
+macro(build_documentation PROJECT_NAME)
+    # Copy some files over
+
     set(RELATIVE_DESTINATION_DIR html)
     set(ABSOLUTE_DESTINATION_DIR ${CMAKE_SOURCE_DIR}/${RELATIVE_DESTINATION_DIR})
     set(FILES)
@@ -17,4 +19,24 @@ macro(build_documentation DOCUMENTATION_NAME)
 
         file(COPY ${ABSOLUTE_DESTINATION_DIR}/${FILE} DESTINATION ${DESTINATION_DIR})
     endforeach()
+
+    # Configure some files
+
+    set(PROJECT_NAME ${PROJECT_NAME})
+
+    if("${PROJECT_NAME}" STREQUAL "libOpenCOR")
+        set(PROJECT_URL "https://opencor.ws/libopencor/index.html")
+        set(PROJECT_DESCRIPTION "the backend library to <a href=\"https://opencor.ws/\">OpenCOR</a>, an <a href=\"licensing.html\">open source</a> <a href=\"supportedPlatforms.html\">cross-platform</a> modelling environment")
+        set(PROJECT_TUTORIAL)
+    else()
+        set(PROJECT_URL "https://opencor.ws/")
+        set(PROJECT_DESCRIPTION "an <a href=\"licensing.html\">open source</a> <a href=\"supportedPlatforms.html\">cross-platform</a> modelling environment (and a replacement for <a href=\"cor/index.html\">COR</a>)")
+        set(PROJECT_TUTORIAL "
+            <ul>
+                <li><a href=\"https://tutorial-on-cellml-opencor-and-pmr.readthedocs.io/en/latest/index.html\">OpenCOR tutorial</a></li>
+            </ul>
+")
+    endif()
+
+    configure_file(${ABSOLUTE_DESTINATION_DIR}/index.html.in ${CMAKE_BINARY_DIR}/${RELATIVE_DESTINATION_DIR}/${ARGN}/index.html)
 endmacro()
